899fa25ae22b21fdc39ab41c193c7bbec9b21264
Leonardo Baggio <leonardo@nexto.com.br>
2017-10-13 16:01:17 -0300

    2

---
+++ src/i18next-net/i18next-net/i18next.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace i18next_net
+{
+    class i18next
+    {
+    }
+}
